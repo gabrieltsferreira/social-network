c9f26b170a41b0097d79c4ec3f36319e4863b6e9
Gabriel Ferreira <gabrieltsferreira@hotmail.com>
2024-01-17 00:41:47 -0300

    added CORS

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,13 @@ const app = express();
 //JSON parser
 app.use(express.json());
 
+//CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+})
 
 //Test Endpoint
 app.get('/test', (req, res) => {
